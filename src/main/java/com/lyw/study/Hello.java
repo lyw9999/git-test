2fcf345efd923e3d633d9a929597e4aef333df7b
lyw <389822622@qq.com>
2022-03-20 21:26:38 +0800

    second commit

--- src/main/java/com/lyw/study/Hello.java
+++ src/main/java/com/lyw/study/Hello.java
@@ -4,5 +4,6 @@ package com.lyw.study;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git11");
     }
 }
